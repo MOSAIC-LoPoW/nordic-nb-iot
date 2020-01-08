43f9015544e57f2f846431f322f60d5ec88ff2da
matsdemeyer <matsdemeyer@gmail.com>
2020-01-08 10:12:10 +0100

    added initial serial_lte_modem_folder

---
+++ serial_lte_modem/build/spm/zephyr/nrf9160_pca10090.dts.pre.d
@@ -0,0 +1,14 @@
+empty_file.o: \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/misc/empty_file.c \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/boards/arm/nrf9160_pca10090/nrf9160_pca10090.dts \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/arm/nordic/nrf9160_sica.dtsi \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/common/mem.h \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/arm/nordic/nrf9160.dtsi \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/arm/armv8-m.dtsi \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/common/skeleton.dtsi \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/include/dt-bindings/i2c/i2c.h \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/include/dt-bindings/gpio/gpio.h \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/dts/arm/nordic/nrf9160_common.dtsi \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/boards/arm/nrf9160_pca10090/nrf9160_pca10090_common.dts \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/zephyr/boards/arm/nrf9160_pca10090/nrf9160_pca10090_partition_conf.dts \
+ /home/mats/Documents/IDLab/nordic-nrf-nbiot/ncs/nrf/samples/nrf9160/spm/nrf9160_pca10090.overlay
